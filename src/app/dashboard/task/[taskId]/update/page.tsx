42136b8ad171538c14298d08d8d313ca6ba68857
KarenOrtiz <karen.ortizg@yahoo.com>
2025-06-02 12:50:17 -0600

    Task Id ?

--- src/app/dashboard/task/[taskId]/update/page.tsx
+++ src/app/dashboard/task/[taskId]/update/page.tsx
@@ -1,14 +1,3 @@
-export default function UpdateTaskPage({
-    params,
-    searchParams,
-  }: {
-    params: { taskId: string };
-    searchParams: { [key: string]: string | string[] | undefined };
-  }) {
-    return (
-      <div>
-        <h1>Actualizar Tarea {params.taskId}</h1>
-        <p>Formulario para registrar progreso</p>
-      </div>
-    );
+export default function UpdateTaskPage() {
+    return <div>Update Task Page</div>;
   }
